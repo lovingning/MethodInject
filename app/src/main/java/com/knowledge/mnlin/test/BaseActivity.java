46ee12c0a53ff786ac6e08e01986b93c72673954
lovingning <357638154@qq.com>
2017-12-18 18:26:16 +0800

    添加makedown文档说明

---
+++ app/src/main/java/com/knowledge/mnlin/test/BaseActivity.java
@@ -0,0 +1,13 @@
+package com.knowledge.mnlin.test;
+
+import com.knowledge.mnlin.methodinject.MethodInjectActivity;
+
+/**
+ * Created on 2017/12/18
+ * function :
+ *
+ * @author ACChain
+ */
+
+public class BaseActivity extends MethodInjectActivity {
+}
---
+++ app/src/main/java/com/knowledge/mnlin/test/BaseApplication.java
@@ -0,0 +1,13 @@
+package com.knowledge.mnlin.test;
+
+import android.app.Application;
+
+/**
+ * Created on 2017/12/18
+ * function :
+ *
+ * @author ACChain
+ */
+
+public class BaseApplication extends Application {
+}
